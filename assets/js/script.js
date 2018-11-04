ee4c9198b8b47aaea2896ce08011b3eb8bbde75a
Ulric Todman <rick.sinclair.nyc@gmail.com>
2018-11-04 00:02:16 -0400

    file structure & succesfully pulling giphy JSON

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,7 @@ to read by non-authors.
 */
 
 //Our topics array will hold all search strings/button labels.
-var topics = ["Thundercats", "He-Man", "Silverhawks", "Voltron", "Thundar"];
+var topics = ["Thundercats", "He  Man", "Voltron", "Thundar"];
 
 //This button div will hold all buttons that we generate.
 var buttonDiv = $("#buttonDiv");
@@ -21,7 +21,8 @@ function generateButtons() {
   //clear all current buttons(as to not generate duplicates)
   $(buttonDiv).empty();
   //loop through every string contained in array topics
-  for (var i in topics) {
+  for (i = 0; i < topics.length; i++) {
+    console.log(topics[i]);
     //create a button using the value of the current index
     var createButton = $("<button></button>", {
       type: "button",
@@ -30,13 +31,11 @@ function generateButtons() {
       //also give it an ID of button+the index number.
       id: "button" + i,
       //btn-spacing was added to our css to space out each button
-      class: "btn btn-info btn-spacing"
+      class: "btn btn-info btn-spacing",
+      value: topics[i]
     });
     //insert the generated button into the div we created to hold the buttons
     $(buttonDiv).append(createButton);
-    $("#button" + i).on("click", function() {
-      giphyAPICall(topics[i]);
-    });
   }
   //remove text from search box after completing button add
   $("#searchBox").val("");
@@ -46,22 +45,34 @@ $("#submitButton").on("click", function() {
   //stop the form from asking for total page reload
   event.preventDefault();
   //append the user defined string into the array at the last(greatest) index.
+  //ot will also remove uneeded whitepace from the front and back of the stsring.
   topics.push(
     $("input[name=search]")
       .val()
       .trim()
   );
+  //function call will replace /regenerate buttons we deleted earlier and
+  //will add the button we made using the text input field.
   generateButtons();
 });
+//this function call will generate the buttons the user sees at page load.
 generateButtons();
 
+//Found solution for syntax here.http://jsfiddle.net/o4fyqvk7/
+//selects buttons in button div "buttonDiv" and queries using their value
+$("#buttonDiv").on("click", "button", function() {
+  giphyAPICall($(this).val());
+});
+
 function giphyAPICall(query) {
-  $.get(
+  var url =
     "https://api.giphy.com/v1/gifs/search?api_key=ZUudWxxOOtymCzKktG3lHi5D88JFvLfQ&q=" +
-      query +
-      "&limit=10&offset=0&lang=en"
-  ).then(function(response) {
+    query +
+    "&limit=25&offset=0&lang=en";
+  console.log(url);
+  $.get(url).then(function(response) {
     var responseLocal = response;
-    $("#results").text(JSON.stringify(responseLocal));
+    console.log(responseLocal);
+    console.log(query);
   });
 }
