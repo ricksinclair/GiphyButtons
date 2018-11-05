274e38918079d3ac281b34501b111d6630c9b383
Ulric Todman <rick.sinclair.nyc@gmail.com>
2018-11-05 14:21:24 -0500

    changes in layout, got stills and pagination down.

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,12 @@ I created a mess of code on that last assignment that made it difficult to follo
 This time around I am going to pseudocode each section to make things easier 
 to read by non-authors.
 */
-
+//this will tell me the item number for each picture/card
+var count = 0;
+//this will be the actual offset that gives us the different pages.
+var offset = 0;
+//i will keep a count of each page. i will multiply i by 25 to get the offset
+var i = 0;
 //Our topics array will hold all search strings/button labels.
 var topics = [
   "Thundercats",
@@ -27,18 +32,18 @@ function generateButtons() {
   //clear all current buttons(as to not generate duplicates)
   $(buttonDiv).empty();
   //loop through every string contained in array topics
-  for (i = 0; i < topics.length; i++) {
-    console.log(topics[i]);
+  for (y = 0; y < topics.length; y++) {
+    console.log(topics[y]);
     //create a button using the value of the current index
     var createButton = $("<button></button>", {
       type: "button",
       //& label it with the text of the current string
-      text: topics[i],
+      text: topics[y],
       //also give it an ID of button+the index number.
-      id: "button" + i,
+      id: "button" + y,
       //btn-spacing was added to our css to space out each button
       class: "btn btn-info btn-spacing",
-      value: topics[i]
+      value: topics[y]
     });
     //insert the generated button into the div we created to hold the buttons
     $(buttonDiv).append(createButton);
@@ -71,14 +76,70 @@ $("#buttonDiv").on("click", "button", function() {
 });
 
 function giphyAPICall(query) {
+  i++;
+
   var url =
     "https://api.giphy.com/v1/gifs/search?api_key=ZUudWxxOOtymCzKktG3lHi5D88JFvLfQ&q=" +
     query +
-    "&limit=25&offset=0&lang=en";
+    "&limit=50&offset=" +
+    offset +
+    "&lang=en";
   console.log(url);
   $.get(url).then(function(response) {
+    //not sure if its redundant to declare the variable or not, but i did
     var responseLocal = response;
+    var results = responseLocal.data;
     console.log(responseLocal);
     console.log(query);
+    console.log("i is: " + i);
+    var div = $("<div>", { id: i, class: "container row" });
+    $("#results").prepend(div);
+
+    for (x = 0; x < 50; x++) {
+      count++;
+      console.log(
+        "index of current object Array is: " +
+          x +
+          " Title is:" +
+          results[x].title
+      );
+      var picture = results[x].images.original_still.url;
+
+      //yes, i did this in one append call.
+
+      $("#" + i).append(
+        "<div id=card" +
+          count +
+          "><img class=card-img-top id=picture" +
+          count +
+          " src=" +
+          picture +
+          "></img><div class=card-body><h2 id=title" +
+          count +
+          ">  " +
+          results[x].title +
+          "</h2> <p class=card-text id=rating" +
+          count +
+          "> Rating: " +
+          results[x].rating +
+          "</p>   <p id=itemnumber" +
+          count +
+          ">  RESULT#" +
+          count +
+          "</p></div></div> "
+      );
+
+      $("#card" + count + "").attr(
+        "class",
+        "card col-sm-6 text-center d-block mx-auto"
+      );
+      $("#title" + count + "").attr("class", "col-12 card-title");
+      $("#picture" + count + "").attr("class", "card-img-top");
+
+      $("#itemnumber" + count + "").attr("class", "col-12 card-text");
+      $("#year" + count + "").attr("class", "col-12 card-text");
+    }
+    $("#" + i + "").append("<h5>PAGE: " + i + "</h5>");
   });
+  offset = i * 25;
 }
